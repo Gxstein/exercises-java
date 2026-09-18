353f16a89836827047edf73e3e2f0ae801658fc1
Gxstein <mxrciodev@outlook.com>
2026-09-18 10:22:31 -0300

    Product scanning

---
+++ src/entities/Product.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totaValueInStock(){
+       return price * quantity;
+    }
+    public void Addproducts (int quantity){
+        this.quantity += quantity;
+        // recebe ele mesmo mais a quantidade
+    }
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+        // recebe ele mesmo menos a quantidade
+    }
+}
