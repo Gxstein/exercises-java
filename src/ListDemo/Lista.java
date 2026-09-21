e13cfe101487036e4556ce08f9ecb636a5f3c2a4
Gxstein <mxrciodev@outlook.com>
2026-09-21 16:01:13 -0300

    ListDemo - List basics: add, remove, removeIf, indexOf, filter and findFirst

---
+++ src/ListDemo/Lista.java
@@ -0,0 +1,52 @@
+package ListDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lista {
+    public static void main(String[] args) {
+        // string list
+        List<String> list = new ArrayList< >();
+
+        list.add("Maria");
+        list.add("Leticia");
+        list.add("Amanda");
+        list.add(0, "Ana");
+
+        // list remove
+        list.remove("Marcos");
+
+        // size list
+        System.out.println(list.size());
+
+        // for each
+        for(String x : list){
+            System.out.println(x);
+        }
+
+        // lambda filter
+        System.out.println("------------------");
+        List<String> result =  list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+
+        for(String x : result){
+            System.out.println(x);
+        }
+
+        //remove by predicate
+        System.out.println("------------------");
+        list.removeIf(x -> x.charAt(0) == 'A');
+        // found index
+        System.out.println("Index of Leticia: " + list.indexOf("Leticia"));
+        // if was removed
+        System.out.println("Index of Marcos: " + list.indexOf("Marcos"));
+        for(String x : list){
+            System.out.println(x);
+        }
+        // find first lambda fuction
+        System.out.println("------------------");
+        String name = list.stream().filter(x -> x.charAt(0) == 'M').findFirst().orElse(null);
+        System.out.println(name);
+
+    }
+}
